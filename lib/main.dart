82d81f3c3ffa7051cb0d8676816201b5a2440789
YueLearningCode <watashinolepdesu@gmail.com>
2026-06-28 19:15:46 +0700

    Revert "Apply purple beige color palette"
    This reverts commit 5a51f3c47311f34308d5dd5da184752dd4a92503.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'app/data/theme/app_colors.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
@@ -17,34 +16,7 @@ void main() async {
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Cermatify",
-      theme: ThemeData(
-        fontFamily: 'Poppins',
-        useMaterial3: false,
-        primaryColor: AppColors.primary,
-        scaffoldBackgroundColor: AppColors.background,
-        colorScheme: const ColorScheme.light(
-          primary: AppColors.primary,
-          secondary: AppColors.primaryLight,
-          surface: AppColors.surface,
-          error: AppColors.error,
-        ),
-        progressIndicatorTheme: const ProgressIndicatorThemeData(
-          color: AppColors.primary,
-        ),
-        checkboxTheme: CheckboxThemeData(
-          fillColor: WidgetStateProperty.resolveWith((states) {
-            return states.contains(WidgetState.selected)
-                ? AppColors.primary
-                : AppColors.surface;
-          }),
-          checkColor: WidgetStateProperty.all(AppColors.surface),
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: AppColors.primary),
-          ),
-        ),
-      ),
+      theme: ThemeData(fontFamily: 'Poppins', useMaterial3: false),
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
